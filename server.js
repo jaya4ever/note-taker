1498bcc658623602c702e5267a6f900f00e1a065
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-09 14:22:20 -0600

    added the middleware to use

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ const api = require('./routes/api');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api',api);
+app.use(express.static('public'));
+
 
