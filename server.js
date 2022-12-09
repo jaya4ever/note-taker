f5b79a9f0b306c6a7f0450a2d11a43c485b35857
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-09 16:48:41 -0600

    gitignore few things, working on the not and api js file

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/notes',(req,res)=>
 );
 
 app.get('*',(req,res)=>
- res.sendFile(path.join(__dirname,'./publicindex.html'))
+ res.sendFile(path.join(__dirname,'./public/index.html'))
 );
 
 app.listen(PORT,()=>
