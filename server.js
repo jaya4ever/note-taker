416b7c8d6bbf6512989af13ad3398bfbb7ffd6e8
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-09 14:18:58 -0600

    defined variables to get it started

---
+++ server.js
@@ -0,0 +1,8 @@
+// defining variables to work on for the application
+const express = require('express');
+const path = require('path');
+const api = require('./routes/api');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
