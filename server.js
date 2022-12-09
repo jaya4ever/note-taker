e855c2930bf8a26ce055bfb3328b67764d01679c
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-09 14:35:48 -0600

    html and api routes are created

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // defining variables to work on for the application
 const express = require('express');
+const req = require('express/lib/request');
 const path = require('path');
 const api = require('./routes/api');
 const PORT = process.env.PORT || 3001;
@@ -9,5 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api',api);
 app.use(express.static('public'));
+
+app.get('/', (req,res)=>
+res.sendFile(path.join(__dirname,'./public/index.html'))
+);
+
+app.get('/notes',(req,res)=>
+ res.sendFile(path.join(__dirname,'./public/notes.html'))
+);
+
+app.get('*',(req,res)=>
+ res.sendFile(path.join(__dirname,'./publicindex.html'))
+);
+
+app.listen(PORT,()=>
+console.log(`App listening at http://localhost:${PORT}`)
+);
+
+
 
 
