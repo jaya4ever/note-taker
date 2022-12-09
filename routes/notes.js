d0115977b80ca7aac61814458e5d6f94067d36e8
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-09 17:18:53 -0600

    working on note.js

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,11 @@ const notes = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const db = require('../db/db.json');
+//const res = require('express/lib/response');
 
-notes.get('/', (req, res)=>
-console.info(`${req.mehod}request received for notes`)
-);
+notes.get('/', (req, res)=>{
+console.info(`${req.method}request received for notes`);
+console.log(db)
+res.json(db);
+
+});
