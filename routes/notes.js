e166b181c4d9479246339b6f7fa9109fcc51f3d6
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-09 19:08:42 -0600

    working in the note.js to add the req and res for the status

--- routes/notes.js
+++ routes/notes.js
@@ -2,6 +2,7 @@ const notes = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const db = require('../db/db.json');
+const { json } = require('express/lib/response');
 //const res = require('express/lib/response');
 
 notes.get('/', (req, res)=>{
@@ -9,4 +10,33 @@ console.info(`${req.method}request received for notes`);
 console.log(db)
 res.json(db);
 
+});
+
+notes.post('./',(req,res)=>{
+    console.info(`${req.method} request to post the reviews`);
+    const{title, text} = req.body;
+    if(title && text){
+        const newNote = {
+            title,
+            text
+        };
+        db.push(newNote);
+        let notesArr = JSON.stringyfy(db);
+
+        fs.writeFile(path.join(__dirname,'../db/db.json'),notesArr, function(err){
+            if(err){
+                throw err;
+            }console.log(`Review the ${newNote.title} that it written in JSON`);
+            
+        });
+        const response = {
+            status: 'success',
+            body:'newNote'
+        };
+        console.log(response);
+        res.status(201).json(response);
+
+    }else{
+        res.status(500).json('having error for the review');
+    }
 });
